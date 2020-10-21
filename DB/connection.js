4c5c569e4801b75a203fa9d9839934001b916d0a
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-10-21 08:07:59 -0700

    DB to db

--- DB/connection.js
+++ DB/connection.js
@@ -5,11 +5,11 @@ require('dotenv').config()
 const { MONGODBURI } = process.env
 const mongoose = require('mongoose')
 const config = {useUnifiedTopology: true, useNewUrlParser: true}
-const DB = mongoose.connection
+const db = mongoose.connection
 mongoose.connect(MONGODBURI, config)
 
 // copied from alex
-DB.on('open', () => console.log('You are connected to Mongo'))
+db.on('open', () => console.log('You are connected to Mongo'))
 	.on('close', () => console.log('You are disconnected to Mongo'))
 	.on('error', (err) => console.log(err));
 
